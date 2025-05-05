e138bf48159404402a6fed52955e678480fd8c5c
Andris Reinman <andris@reinman.eu>
2025-05-05 20:05:29 +0300

    pdate

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,57 +60,76 @@ export default {
     },
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
-      image: "img/social-card.png",
-      navbar: {
-        title: "Nodemailer",
-        logo: {
-          alt: "Nodemailer",
-          src: "img/nm_logo_200x136.png",
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/social-card.png",
+    navbar: {
+      title: "Nodemailer",
+      logo: {
+        alt: "Nodemailer",
+        src: "img/nm_logo_200x136.png",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "docs",
+          position: "left",
+          label: "Documentation",
         },
-        items: [
-          {
-            type: "docSidebar",
-            sidebarId: "docs",
-            position: "left",
-            label: "Documentation",
-          },
-          {
-            href: "https://emailengine.app",
-            position: "left",
-            label: "EmailEngine",
-          },
-          {
-            href: "https://www.npmjs.com/package/nodemailer",
-            label: "NPM",
-            position: "right",
-          },
-          {
-            href: "https://github.com/nodemailer/nodemailer",
-            label: "GitHub",
-            position: "right",
-          },
-        ],
-      },
-      footer: {
-        style: "dark",
+        {
+          href: "https://emailengine.app",
+          position: "left",
+          label: "EmailEngine",
+        },
+        {
+          href: "https://www.npmjs.com/package/nodemailer",
+          label: "NPM",
+          position: "right",
+        },
+        {
+          href: "https://github.com/nodemailer/nodemailer",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
 
-        copyright: `Copyright © 2010 - ${new Date().getFullYear()} Andris Reinman. Powered by <a href="https://emailengine.app" class="footer__link-item">EmailEngine</a>.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ["php"],
-      },
-      defaultMode: "light",
-      disableSwitch: false,
-      respectPrefersColorScheme: false,
+      copyright: `Copyright © 2010 - ${new Date().getFullYear()} Andris Reinman. Powered by <a href="https://emailengine.app" class="footer__link-item">EmailEngine</a>.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+      additionalLanguages: ["php"],
+    },
+    defaultMode: "light",
+    disableSwitch: false,
+    respectPrefersColorScheme: false,
 
-      mermaid: {
-        theme: { light: "default", dark: "dark" },
-      },
-    }),
+    mermaid: {
+      theme: { light: "default", dark: "dark" },
+    },
+
+    algolia: {
+      // The application ID provided by Algolia
+      appId: "BNTKMOXVM6",
+
+      // Public API key: it is safe to commit it
+      apiKey: "8b9aa4293a38493456a7797f8f1c3a82",
+
+      indexName: "nodemailer",
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: "search",
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: true,
+
+      //... other Algolia params
+    },
+  },
 };
